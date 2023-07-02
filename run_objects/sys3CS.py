36d3f724e3c17a406a75735160e15131971f7b18
Hargy <95535236+nhargy@users.noreply.github.com>
2023-07-02 18:05:14 +0300

    Completed sys3CS object.
    Built get_power() function, and apply_rules() function. Also created a /rule directory to hold the rules json files.

--- run_objects/sys3CS.py
+++ run_objects/sys3CS.py
@@ -9,9 +9,10 @@ from auxil.auxil import read_solis_txt
 from datetime import datetime
 import os
 import time
+import json
 
 # PATHS
-path_to_rules = 'C:/Data3CS/Configs/Rules'; default_rule = f'{path_to_rules}/rule_AA.json'
+path_to_rules = 'C:/rep_py3cs/py3cs/rules'; default_rule = f'{path_to_rules}/rule_AA.json'
 
 # DEFAULT SLIT
 mono_slit     = 1000
@@ -233,6 +234,8 @@ class sys3CS:
             logprint(f"Retrieved system state: {tup_state}", self.log_target, toprint=False)
 
             return tup_state, dt_state
+
+
 
     def ctrl(self,
              source_power           = None,
@@ -509,8 +512,9 @@ class sys3CS:
                     try:
                         tup_state, dt_state            = self.get_state(get_tuple=True)
                         solis_tuple, dt_solis, np_data = read_solis_txt(spec_save)
+                        power_sample                   = self.get_power(pm_b=True)[1]
                         logprint("Extracted data and metadata", self.log_target)
-                        return solis_tuple, tup_state, np_data
+                        return solis_tuple, tup_state, power_sample, np_data
                     except Exception as e:
                         logprint("FAILED to extract data and metadata", self.log_target)
                         logprint(e, self.log_target)
@@ -566,4 +570,101 @@ class sys3CS:
                     except:
                         logprint("Failed to issue command", self.log_target)
                 else:
-                    logprint("INPUT ERROR: pm_t_wl must be an integer", self.log_target) 
+                    logprint("INPUT ERROR: pm_t_wl must be an integer", self.log_target)
+
+
+
+    def get_power(self, pm_a = False, pm_b = False, pm_t = False, wl_agree = True, samples = 3):
+        
+        logprint("Getting power", self.log_target)
+        
+        wl = self.mono_wl
+        
+        logprint(f"mono_wl is {wl}", self.log_target)
+        
+        if pm_a == True:
+            if wl_agree == True:
+                self.ctrl(pm_a_wl=wl)
+
+            sum_a = 0
+            try:
+                logprint("Getting measurement for pm_a", self.log_target)
+                for i in range(0, samples):
+                    sample = self.s.power_meter_a.power
+                    sum_a+= sample
+                sample_a = sum_a/samples
+                logprint(f"Done. Average measurement: {sample_a}", self.log_target)
+            except:
+                logprint("FAILED to get power for pm_a", self.log_target)
+                sample_a = None
+        else:
+            sample_a = None
+            
+        if pm_b == True:
+            if wl_agree == True:
+                self.ctrl(pm_b_wl=wl)
+
+            sum_b = 0
+            try:
+                logprint("Getting measurement for pm_b", self.log_target)
+                for i in range(0, samples):
+                    sample = self.s.power_meter_b.power
+                    sum_b+= sample
+                sample_b = sum_b/samples
+                logprint(f"Done. Average measurement: {sample_b}", self.log_target)
+            except:
+                logprint("FAILED to get power for pm_b", self.log_target)
+                sample_b = None
+        else:
+            sample_b = None
+            
+        if pm_t == True:
+            if wl_agree == True:
+                self.ctrl(pm_t_wl=wl)
+
+            sum_t = 0
+            try:
+                logprint("Getting measurement for pm_t", self.log_target)
+                for i in range(0, samples):
+                    sample = self.s.power_meter_t.power
+                    sum_t+= sample
+                sample_t = sum_t/samples
+                logprint(f"Done. Average measurement: {sample_t}", self.log_target)
+            except:
+                logprint("FAILED to get power for pm_t", self.log_target)
+                sample_t = None
+        else:
+            sample_t = None
+            
+        return sample_a, sample_b, sample_t
+
+
+
+    def apply_rules(self, exc_wl, rule_filepath = default_rule):
+    
+        logprint(f"Applying rules from {rule_filepath}", self.log_target)
+        
+        self.ctrl(mono_wl=exc_wl)
+        
+        # extract rules from file
+        with open(rule_filepath) as rules:
+            data   = json.load(rules)
+            devices  = data['devices']
+            ranges   = data['ranges']
+            states   = data['states']
+            
+        # loop thorugh to apply correct states according to exc_wl
+        for i in range(len(ranges)-1):
+            if ranges[i] <= exc_wl < ranges[i+1]:
+                for j in range(len(devices)):
+                    device    = devices[j]
+                    value     = states[i][j]
+                    arguments = {device:value}
+                    try:
+                        self.ctrl(**arguments)
+                    except:
+                        logprint(f'Did not apply rules from {rule_filepath} to {device} for required value {value}',self.log_target)
+            
+        logprint(f'Rules applied.',self.log_target)
+    
+        return None
